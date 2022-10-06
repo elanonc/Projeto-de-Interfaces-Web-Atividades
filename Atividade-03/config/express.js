79f9152c008d32da4f299feefd20aac13a50a43c
Elano N. Caitano <nunes.elano14@gmail.com>
2022-10-05 23:36:35 -0300

    Atividade 03

---
+++ Atividade-03/config/express.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const routes_usuario = require("../app/route/usuario.route");
+const router_comentarios = require("../app/route/comentario.route");
+const router_post = require("../app/route/post.route");
+
+module.exports = function(){
+    let app = express();
+    app.set("port", 3000);
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false })); 
+    routes_usuario(app);
+    router_comentarios(app);
+    router_post(app);
+    app.use(express.static("./public"))
+    return app;
+}
